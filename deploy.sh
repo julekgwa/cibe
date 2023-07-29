aae29089ec145cea1e5fef7a81e0772421510f34
Junius Lekgwara <phutigravel@gmail.com>
2023-07-29 21:02:14 +0200

    updated circle ci

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-# #replace this with the path of your project on the VPS
-# cd ~/circleci-test
-
-# #pull from the branch
-# git pull origin main
-
-# # followed by instructions specific to your project that you used to do manually
-# npm install
-# export PATH=~/.npm-global/bin:$PATH
-# source ~/.profile
-
-# pm2 restart app
-docker compose up -d --build --force-recreate
+docker compose up --scale api=3 -d --build --force-recreate
